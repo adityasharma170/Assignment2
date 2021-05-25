e67329f771afd8ad77eab5c4aaed6233e1eeeec3
adityasharma170 <84501285+adityasharma170@users.noreply.github.com>
2021-05-25 08:37:16 +0530

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AssignmentLibrary;
+
+namespace ConsoleApp3
+{
+    class Program
+    {   
+       
+
+        static void Main(string[] args)
+        {
+            DateTime fromdate = TimeAndDate.FromDate();
+            Console.WriteLine(fromdate);
+            Console.WriteLine("----------------------------");
+            DateTime todate =TimeAndDate.ToDate();
+            Console.WriteLine(todate);
+          
+            if (fromdate< DateTime.Now)
+            {
+                if (todate>= fromdate)
+                {
+                   TimeAndDate.Calculation( todate, fromdate);
+                }
+                else
+                {
+                    todate=TimeAndDate.ToDate();
+                    Console.WriteLine(todate);
+                    TimeAndDate.Calculation( todate, fromdate);
+                }
+            }
+            else
+            {
+                Console.Write("FromDate is greater than today'date .Try Again");
+            }
+            }
+        }
+    }
